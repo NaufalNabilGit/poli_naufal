607552b45c47e0c7c56440da09926aaf2edfb6bb
NaufalNabilGit <naufal30679@gmail.com>
2024-01-06 10:25:10 +0700

    v3

--- components/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Semoga Cepat Sembuh!
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Naufal Nabil Nurherdian</a>.</strong> All rights reserved.
-  </footer>
